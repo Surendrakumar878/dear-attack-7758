c5ab5ddbb21f7b42da43f5be33a61b44286fe616
surendra kumar <surendramarshkole78@gmail.com>
2023-01-14 16:47:49 +0530

    aad

---
+++ dear-attack/src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import { MainRoutes } from './page/MainRoutes';
+
+// import { HomePage } from './page/HomePage';
+
+import { Box } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+function App() {
+  
+  
+  return (
+<Box>
+
+
+
+<Box pt="100px">
+
+    <MainRoutes/>
+</Box>
+   
+</Box>
+  
+    
+ 
+  
+  );}
+
+
+export default App;
+
---
+++ dear-attack/src/page/HomePage.jsx
@@ -0,0 +1,22 @@
+import {
+  Box,
+  Button,
+  Center,
+  Grid,
+  GridItem,
+  Image,
+  Text,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+
+
+export const HomePage = () => {
+  
+  return (
+    <Box  w={{ lg: "100%", md: "100%", sm: "100%" }}>
+     HomePage
+    </Box>
+  );
+};

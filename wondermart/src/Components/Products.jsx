61bfcf6c388fa545374234ea59ce611d2b15c2cf
AdityaBr11 <adityaanandpurnea1@gmail.com>
2023-01-19 23:45:33 +0530

    product page done

--- wondermart/src/Components/Products.jsx
+++ wondermart/src/Components/Products.jsx
@@ -145,7 +145,7 @@ const test=[
 const Products = () => {
   return (
     <div className="w-full">
-      <div className="w-[100%] md:w-[100%] lg:w-[80%] mx-auto border border-red-500 mt-5">
+      <div className="w-[100%] md:w-[100%] lg:w-[80%] mx-auto mt-5">
         <div className="px-2">
           <Breadcrumb
             spacing="3px"
@@ -193,7 +193,7 @@ const Products = () => {
         {/* main section */}
         <div className="flex mt-8">
           {/* category section */}
-          <div className="border border-red-500 hidden md:block md:w-1/5 mb-10">
+          <div className="border-r-2 hidden md:block md:w-1/5 mb-10">
             <div>
               {/* cat filter */}
               <div className="mt-5 mx-2 border-b-2">
@@ -226,7 +226,7 @@ const Products = () => {
               </div>
               <div className="border flex items-center justify-between px-2 border-gray-300 mt-1  py-1 md:text-xs lg:text-sm w-[100%]">
                 <input
-                  className="outline-none text-gray-500"
+                  className="outline-none text-gray-500 "
                   type="text"
                   placeholder="Search by Brand"
                 />
@@ -239,7 +239,7 @@ const Products = () => {
                 <div className="px-1 mt-1 text-xs lg:text-sm text-gray-500 overflow-y-scroll">
                   <div className="text-sm flex items-center gap-2 mt-2">
                     <input
-                      className="w-[15px] h-[15px]"
+                      className="w-[15px] h-[15px] rounded-none"
                       type="checkbox"
                       value=""
                       name=""
@@ -558,7 +558,7 @@ const Products = () => {
           </div>
 
           {/* products sections */}
-          <div className="border border-black w-[96%] mx-auto md:w-4/5 px-4">
+          <div className="w-[96%] mx-auto md:w-4/5 px-4">
             <div className="flex justify-between items-center mt-4">
               <div>
                 <h1 className="text-xs md:text-2xl text-gray-600">
@@ -610,8 +610,30 @@ const Products = () => {
                   ))
                 }
             </div>
-
+            <h1 className="text-center mt-3 text-gray-500">--That's all folks--</h1>
           </div>
+        </div>
+      </div>
+
+      <div className="w-[100%] lg:w-[80%] mx-auto bg-gray-200 py-4 mt-5 shadow-xl px-4 ">
+        <div className="text-gray-600">
+          <h1 className="text-xl">Buy Fruits and Vegetables Online</h1>
+          <p className="mt-2 text-sm md:text-md leading-6">While fresh vegetables should be a part of your everyday meals, some vegetables are hard to find even in the huge supermarkets in your area. However, the luxury of online shopping offers customers the comfort of relishing these veggies by ordering them from the comfort of their home. While beetroot, amlas, green peas, broccoli can easily be found in the market, bigbasket makes grocery shopping easier for you with their fresh produce delivered in top class packages right at your doorstep. With their wide delivery network, geographical barriers are easily overcome in more than 25 major cities, thereby giving you the benefit of fresh fruits and vegetables every time.</p>
+        </div>
+
+        <div className="text-gray-600 mt-4">
+          <h1 className="text-xl">Fruits and Vegetables</h1>
+          <p className="mt-2 text-sm md:text-md leading-6">Fruits and vegetables form the base of everybody’s daily food consumption. However, not everyone has access to fresh and healthy vegetables and fruits due to various reasons. Starting from farming methods that involve toxicants to storage systems and logistics, many sellers land up with rotten raw foods or foods exposed to chemicals and other radicals. Bigbasket has now turned the scene around with its online shopping store, where you can have fresh potato, tomato, carrots, onions and everything else on your grocery list, delivered to your doorstep, even without stepping out of your house. Apart from your regular local produce, you can also shop for other seasonal produces such as asparagus, broccoli, drumstick, white onion and more on bigbasket’s online store. Packaging plays a vital role in up keeping the freshness of your fruits and vegetables. Green leafy vegetables are prone to spoilage easily if not handled properly. While ordering online in bigbasket, worries about packaging and hygiene can be kept at bay. Be it your lettuce, cauliflower or any green vegetable, the packaging standard is specific and of high quality. You can find a whole wide range of vegetables such as red cabbage, drumstick leaves and different exotic ones as well on bigbasket. Apart from quality, we also believe in making fresh foods available to all and thus, you can find very nominal pricing while shopping online with us. Comapre the onion price or potato price with anywhere else in the market, and you will understand what we are talking about. Quality is uncompromised regardless of the type of delivery that you choose, standard or express. The produce delivered to you will be as fresh as directly cultivated from the farms resulting in high levels of nutrition. You no more have to walk through aisles of piled up produce and spend time carefully picking out each vegetable or fruit from them. We will take care of the entire process for you. We at bigbasket, cautiously pick your produce, meticulously clean them and pack them using the best packaging material available before delivering them to you. Shop for your vegetables and fruits online now from the comfort of your couch and have your order delivered to your doorstep within a stipulated time. Look out for great deals, offers and value packs while shopping online with us. Save time, money and efforts, by completing your routine grocery shopping on bigbasket.</p>
+        </div>
+
+        <div className="text-gray-600 mt-4">
+          <h1 className="text-xl">Freshness- the promise of quality</h1>
+          <p className="mt-2 text-sm md:text-md leading-6">The freshness of the produce being delivered to you is unquestionable, even in the case of standard delivery. Veggies like banana stem and banana flower, which are used in exotic dishes, are fresh and readily available to buy online at bigbasket. Vegetables like mushrooms and sprouts, which are prone to quick spoilage are also packed with utmost care using the best materials. Variety of vegetables such as button mushrooms, raw banana and yellow zucchini are also available. With the demand for organic produce continuously rising, bigbasket has made them readily accessible. You can shop for organic bananas online on the site too. All these fruits and vegitables are brought from verified and reputed vendors, giving no room to worry about quality. Striving to sustain the nutritive benefits of the vegetables and fruits, bigbasket takes its packaging very seriously and abides by a very strict packing standard. With a whole lot of groceries to shop for and the luxury of at-home shopping, bigbasket brings convenience to your grocery shopping experience with regard to time, delivery, cost and quality. bigbasket offers a range of prominent products that are well received by customers. “Zespri” is one such well-known brand that is available to shop for online. For those looking out for organic products only, bigbasket stocks products by TVS Organic too. Make bigbasket your prime choice to buy fruits and vegetables online.</p>
+        </div>
+
+        <div className="text-gray-600 mt-4">
+          <h1 className="text-2xl">Delivery in Major Cities</h1>
+          <p className="mt-2 text-sm md:text-md leading-6">We at bigbasket choose our produce with as much care as you do. Carefully sorted, meticulously cleaned and carefully packed, fruits and vegetables from our online store reach you the day you order them. bigbasket offers delivery in Chennai, delivery in Delhi and most other major cities in India.</p>
         </div>
       </div>
     </div>

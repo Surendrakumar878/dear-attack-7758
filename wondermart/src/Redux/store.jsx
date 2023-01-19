244d5918e9aeb6764a15f948ed716645ab55697c
surendra kumar <surendramarshkole78@gmail.com>
2023-01-19 16:44:15 +0530

    a

--- wondermart/src/Redux/store.jsx
+++ wondermart/src/Redux/store.jsx
@@ -1,1 +1,15 @@
-// store
+import { applyMiddleware, combineReducers, compose, legacy_createStore } from 'redux'
+import { reducer as AppReducer } from './AppReducer/reducer'
+import { reducer as AuthReducer } from './AuthReducer/reducer'
+import thunk from 'redux-thunk'
+
+
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+
+const rootReducer = combineReducers({AppReducer,AuthReducer,})
+
+
+
+export const store = legacy_createStore(rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+)

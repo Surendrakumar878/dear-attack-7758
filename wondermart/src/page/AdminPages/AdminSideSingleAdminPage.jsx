b8e189441139a056bacd0cf14166fdb94d50e0f5
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2023-01-20 18:04:28 +0530

    fw20_0784_day-4

--- wondermart/src/page/AdminPages/AdminSideSingleAdminPage.jsx
+++ wondermart/src/page/AdminPages/AdminSideSingleAdminPage.jsx
@@ -1,8 +1,27 @@
 import admin from "../AdminAssets/admin.jpg";
-import "../../Components/AdminStyles/AdminSideSingleAdminPage.css";
+import "../AdminStyles/AdminSideSingleAdminPage.css";
 import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import { useDispatch } from "react-redux";
+import { useToast } from "@chakra-ui/react";
+import { deleteUser, getUsers } from "../../Redux/adminReducer/action";
 
 const AdminSideSingleAdminPage = ({ username, email, id, adminID }) => {
+
+    const dispatch = useDispatch();
+    const toast = useToast();
+
+    const deleteAdminHandler = (id) => {
+        dispatch(deleteUser(id))
+            .then(() => toast({
+                title: 'Admin deleted !',
+                description: "We've deleted admin.",
+                status: 'success',
+                duration: 1500,
+                position: "top",
+                isClosable: true,
+            }))
+            .then(() => dispatch(getUsers()));
+    };
 
     return (
         <div className="single_admin_container">
@@ -15,9 +34,8 @@ const AdminSideSingleAdminPage = ({ username, email, id, adminID }) => {
             </div>
             <div className="admin_single_bottom_section">
                 <div>
-                    <EditIcon fontSize="130%" cursor="pointer" />
                 </div>
-                <div className="color_red"><DeleteIcon fontSize="130%" cursor="pointer" /></div>
+                <div className="color_red"><DeleteIcon onClick={() => deleteAdminHandler(id)} fontSize="130%" cursor="pointer" /></div>
             </div>
         </div>
     );

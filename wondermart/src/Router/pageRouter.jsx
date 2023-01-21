1cdb6ef04384ea50c5c09d64b1fef98d9d6ff6d5
surendrakumar878 <surendramarshkole78@gmail.com>
2023-01-21 16:53:20 +0530

    router changes

--- wondermart/src/Router/pageRouter.jsx
+++ wondermart/src/Router/pageRouter.jsx
@@ -2,21 +2,33 @@ import React from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Cart from '../Components/Cart'
 import HomePage from '../Components/HomePage'
-
-import { Navbar } from '../Components/navabar/Navbar'
-
+// import { Navbar } from '../Components/navabar/Navbar'
+import Products from '../Components/Products'
+import { AdminSideHomePage } from "../page/AdminPages/AdminSideHomePage";
+import { AdminSideProducts } from "../page/AdminPages/AdminSideProducts";
+import { AdminSideAddProducts } from "../page/AdminPages/AdminSideAddProducts";
+import { AdminSideProductEditPage } from "../page/AdminPages/AdminSideProductEditPage";
+import  Login from '../page/login/Login'
+import { Signup } from '../page/signup/Signup'
+import Navbar from '../Components/navabar/Subnavbar'
 
 function PageRouter() {
   return (
     <Routes>
         <Route path="/" element={<HomePage/>}/>
-        
+        Navbar
         <Route path="/nav" element={<Navbar/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/products" element={<Products/>}/>
+        <Route path="/admin_home_page" element={<AdminSideHomePage />} />
+        <Route path='/admin_side_products' element={<AdminSideProducts />} />
+        <Route path="/admin_side_add_product" element={<AdminSideAddProducts/>}/>
+        <Route path="/admin_side_product_edit_page" element={<AdminSideProductEditPage/>}/>
 
     </Routes>
   )
 }
 
 export default PageRouter
-
-
